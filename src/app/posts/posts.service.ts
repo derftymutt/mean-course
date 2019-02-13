6ac6ac08da8a258d5233021c04ddec36f134b3d4
Allen Bleyle <derftymutt@gmail.com>
2019-02-13 14:46:54 -0800

    finshed with section 3 (adding node)

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -23,7 +23,10 @@ export class PostsService {
    }
 
    addPost(post: Post) {
-     this.posts.push(post);
-     this.postsUpdated.next([...this.posts]);
+     this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+      .subscribe(response => {
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
    }
 }
